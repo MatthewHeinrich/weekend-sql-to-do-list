fd2e1e9c358be197be6b2a6d2c41c9a1a35d9a68
MatthewHeinrich <mheinrich95@gmail.com>
2021-03-19 14:20:37 -0500

    set up rough GET and POST routes

--- server/modules/routes/todo_route.js
+++ server/modules/routes/todo_route.js
@@ -1,8 +1,25 @@
 // require
+const express = require( 'express' );
+const router = express.Router();
+
+const pool = require('../pool');
 
 // GET
+router.get( '/', ( req, res )=>{
+    console.log( 'in route GET' );
+}).catch( ( error )=>{
+    console.log( error )
+    res.sendStatus( 500 );
+}) // end GET route
 
 // POST
+router.post( '/', ( req, res )=>{
+    console.log( 'in route POST', req.body );
+    res.sendStatus( 200 );
+}).catch( ( error )=>{
+    console.log( error );
+    res.sendStatus( 500 );
+})
 
 // PUT
 
@@ -10,3 +27,4 @@
 
 
 // export
+module.exports = router;
--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,12 +1,41 @@
 $( document ).ready( onReady );
 
 function onReady(){
-    console.log('JQ')
-    getTasks();
+    $('#submitButton').on('click', addTask );
+    // getTasks();
 } // end onReady
 
-
+function addTask(){
+    console.log( 'in addTask' );
+    let taskToAdd = {
+        task: $('#createTask').val(),
+        complete: false
+    }
+    // send object to server
+    $.ajax({
+        method: 'POST',
+        url: '/tasks',
+        data: taskToAdd
+    }).then( function ( response ){
+        console.log( 'in POST with:', response );
+        
+    }).catch( function ( error ){
+        console.log( error );
+        alert('Cannot add task');
+    })
+} // end addTask
 
 function getTasks(){
+    console.log( 'in getTask' );
 
+    $.ajax({
+        method: 'GET',
+        url: '/tasks'
+    }).then( function ( response ){
+        console.log( 'back from GET:', response );
+
+    }).catch( function ( error ){
+        console.log( error );
+        alert('sorry cannot get tasks')
+    })
 } // end getTasks
